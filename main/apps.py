075ebefe6b4c51833d1f2132b7bfdaf15b9f1df5
hanifahreza <hanifahreza@pop-os.localdomain>
2022-12-11 13:01:32 +0700

    add nltk download stopwords and punkt

--- main/apps.py
+++ main/apps.py
@@ -1,6 +1,11 @@
 from django.apps import AppConfig
+import nltk
 
 class MainConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'main'
+
+    def ready(self):
+        nltk.download('stopwords')
+        nltk.download('punkt')
     
